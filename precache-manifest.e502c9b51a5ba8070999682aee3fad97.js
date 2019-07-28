3adc08e36aadf4f009f1c4d2cc118596156cf1d7
a3t3mbk <a3t3mbk@gmail.com>
2019-07-28 18:25:34 +0900

    webアプリホストに変更
    Progateで勉強したときのままだったので、a3t3mbk.comと同一の内容をホストしてあります。

---
+++ precache-manifest.e502c9b51a5ba8070999682aee3fad97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd284e9877a4f8c6b7f3832735e66c49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2602b3e6cf9c831de4ec",
+    "url": "/static/css/2.9d7c321c.chunk.css"
+  },
+  {
+    "revision": "b66362a04cf6f10b7951",
+    "url": "/static/css/main.2fe16128.chunk.css"
+  },
+  {
+    "revision": "2602b3e6cf9c831de4ec",
+    "url": "/static/js/2.b24baf64.chunk.js"
+  },
+  {
+    "revision": "b66362a04cf6f10b7951",
+    "url": "/static/js/main.7be62b50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13d51a0969eb840ac021879c92f187ce",
+    "url": "/static/media/genmai.13d51a09.jpg"
+  },
+  {
+    "revision": "c089f1526b416a0b0180c8787a5653d6",
+    "url": "/static/media/logo-128.c089f152.jpg"
+  },
+  {
+    "revision": "e3d59086a25b757ac98fbe5c20a490e3",
+    "url": "/static/media/onigiri_maru.e3d59086.png"
+  }
+]);
